6d946eed42c19828a2d935ee6c3d133f6369a808
Alan Fránquez <santifranquez@gmail.com>
2025-10-21 10:44:20 -0300

    Maui

---
+++ AppNetCredenciales/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppNetCredenciales
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppNetCredenciales/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AppNetCredenciales
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
